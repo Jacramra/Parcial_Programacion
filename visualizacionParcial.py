abbe29bc7e3cd34e4a2077c9b973407dfb6ecd6f
Jacramra <63827359+Jacramra@users.noreply.github.com>
2025-05-25 01:29:32 -0500

    Adicionando campo Nombre operario.

--- visualizacionParcial.py
+++ visualizacionParcial.py
@@ -2,14 +2,20 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import tkinter as tk
+from tkinter import messagebox
 
 ventana = tk.Tk()
 ventana.title("Control de la Producción en Panadería")
 
 # 1. Registro de producción diaria
 def registrar_produccion():
-    nombreOperario = label_nombreOperario.get().strip()
-    messagebox.showinfo("Registrado", f"{nombre} registrado con eficiencia {eficiencia} ({estado})")
+    nombre = entry_nombreOperario.get().strip()
+
+    if not nombre:
+        messagebox.showwarning("Falta nombre", "Por favor, ingrese el nombre del operario.")
+        return
+    messagebox.showinfo("Registrado", f"Nombre: {nombre}.")
+    #messagebox.showinfo("Registrado", f"{nombre} registrado con eficiencia {eficiencia} ({estado})")
 
 # 4. Cierra la ventana
 # Referencia: 
@@ -19,6 +25,7 @@ def cerrar_ventana():
 
 
 # Menú
+# Botón de registro
 boton_registrar = tk.Button(ventana, text="Registrar producción", command=registrar_produccion)
 boton_registrar.grid(row=4, column=0)
 
@@ -31,17 +38,12 @@ boton_reporteIndividual.grid(row=6, column=0)
 boton_Salir = tk.Button(ventana, text="Salir", command=cerrar_ventana) 
 boton_Salir.grid(row=7, column=0)
 
-
-
-
-
-
 #Campo: Nombre operario
-#label_nombreOperario = tk.Label(ventana, text="Nombre del operario")
-#label_nombreOperario.grid(row=0, column=0)
+label_nombre = tk.Label(ventana, text="Nombre del operario")
+label_nombre.grid(row=0, column=0)
 
-#label_nombreOperario = tk.Entry(ventana)
-#label_nombreOperario.grid(row=0, column=1)
+entry_nombreOperario = tk.Entry(ventana)
+entry_nombreOperario.grid(row=0, column=1)
 
 
 
