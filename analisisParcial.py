14ac16c53bcbce1e1a91211d1d0a0c73347042a8
Jacramra <63827359+Jacramra@users.noreply.github.com>
2025-05-26 09:23:25 -0500

    Actualizando Analisis.py

--- analisisParcial.py
+++ analisisParcial.py
@@ -4,3 +4,26 @@ import matplotlib.pyplot as plt
 import random
 
 
+# ________________________________________________
+# ***Funciones Tarea: proyectos de construcción***
+# ________________________________________________
+
+class Panaderia:
+
+    # 1. Registro de producción diaria
+    def registrar_produccion():
+        nombre = entry_nombreOperario.get().strip()         # Obtiene el texto ingresado y quitar espacios extra
+
+        if not nombre:
+            messagebox.showwarning("Falta nombre", "Por favor, ingrese el nombre del operario.")
+            return
+        messagebox.showinfo("Registrado", f"Nombre: {nombre}.")
+        #messagebox.showinfo("Registrado", f"{nombre} registrado con eficiencia {eficiencia} ({estado})")
+
+        # Oculta los widgets relacionados para limpiar la interfaz
+        label_nombre.grid_remove()              # Oculta la etiqueta
+        entry_nombreOperario.grid_remove()      # Oculta el campo de texto
+        boton_confirmar.grid_remove()           # Oculta el botón confirmar
+        entry_nombreOperario.delete(0, tk.END)  # Limpia el campo
+
+   
--- visualizacionParcial.py
+++ visualizacionParcial.py
@@ -3,6 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import tkinter as tk
 from tkinter import messagebox
+from analisisParcial import Panaderia
+
 
 ventana = tk.Tk()
 ventana.title("Control de la Producción en Panadería")
@@ -13,51 +15,39 @@ ventana.title("Control de la Producción en Panadería")
 
 def mostrar_campos(): # Crear el label y entry solo cuando se pulse el botón
     
-    # Campo Nombre Operario
+    # Campo: Nombre Operario
     label_nombre = tk.Label(ventana, text="Nombre del operario")
     label_nombre.grid(row=1, column=0)                  # Mostrar etiqueta 
     entry_nombreOperario = tk.Entry(ventana)
     entry_nombreOperario.grid(row=1, column=1)          # Mostrar campo de texto
 
-    # Campo Cant. Pan Francés
+    # Campo: Cant. Pan Francés
     label_panFrances = tk.Label(ventana, text="Cant. pan francés")
     label_panFrances.grid(row=2, column=0)              # Mostrar etiqueta 
     entry_pf = tk.Entry(ventana)
     entry_pf.grid(row=2, column=1)                      # Mostrar campo de texto
 
-    # Campo Cant. Pan Queso
+    # Campo: Cant. Pan Queso
     label_panQueso = tk.Label(ventana, text="Cant. pan de queso")
     label_panQueso.grid(row=3, column=0)                # Mostrar etiqueta 
     entry_pq = tk.Entry(ventana)
     entry_pq.grid(row=3, column=1)                      # Mostrar campo de texto
 
+    # Campo: Cant. Pan Croissants
+    label_panCroissants = tk.Label(ventana, text="Cant. pan de croissants")
+    label_panCroissants.grid(row=4, column=0)           # Mostrar etiqueta 
+    entry_cr = tk.Entry(ventana)
+    entry_cr.grid(row=4, column=1)                      # Mostrar campo de texto
 
+    # Botón: Confirmar registro de información
+    boton_confirmar.grid(row=5, column=0, columnspan=2) 
 
-    boton_confirmar.grid(row=5, column=0, columnspan=2) # Mostrar botón "Confirmar registro"
-
-
-
-# 1. Registro de producción diaria
-def registrar_produccion():
-    nombre = entry_nombreOperario.get().strip()         # Obtiene el texto ingresado y quitar espacios extra
-
-    if not nombre:
-        messagebox.showwarning("Falta nombre", "Por favor, ingrese el nombre del operario.")
-        return
-    messagebox.showinfo("Registrado", f"Nombre: {nombre}.")
-    #messagebox.showinfo("Registrado", f"{nombre} registrado con eficiencia {eficiencia} ({estado})")
-
-    # Oculta los widgets relacionados para limpiar la interfaz
-    label_nombre.grid_remove()              # Oculta la etiqueta
-    entry_nombreOperario.grid_remove()      # Oculta el campo de texto
-    boton_confirmar.grid_remove()           # Oculta el botón confirmar
-    entry_nombreOperario.delete(0, tk.END)  # Limpia el campo
-
-# 4. Cierra la ventana
+ # 4. Cierra la ventana
 # Referencia: 
-# GeeksforGeeks. (2022). Destroy method in Tkinter Python. Disponible en: https://www.geeksforgeeks.org/destroy-method-in-tkinter-python/
+ # GeeksforGeeks. (2022). Destroy method in Tkinter Python. Disponible en: https://www.geeksforgeeks.org/destroy-method-in-tkinter-python/
 def cerrar_ventana():
     ventana.destroy()
+
 
 #_________________________________________________________________________________________
 # _______________________________________MENÚ_____________________________________________
@@ -68,18 +58,15 @@ boton_registrar = tk.Button(ventana, text="Registrar producción", command=mostr
 boton_registrar.grid(row=4, column=0)
 
 # Botón para confirmar el registro (inicialmente oculto)
-boton_confirmar = tk.Button(ventana, text="Confirmar registro", command=registrar_produccion)
+boton_confirmar = tk.Button(ventana, text="Confirmar registro", command=mostrar_campos)
 
-boton_reporteGeneral = tk.Button(ventana, text="Reporte General", command=registrar_produccion)
+boton_reporteGeneral = tk.Button(ventana, text="Reporte General", command=mostrar_campos)
 boton_reporteGeneral.grid(row=5, column=0)
 
-boton_reporteIndividual = tk.Button(ventana, text="Reporte Individual", command=registrar_produccion)
+boton_reporteIndividual = tk.Button(ventana, text="Reporte Individual", command=mostrar_campos)
 boton_reporteIndividual.grid(row=6, column=0)
 
 boton_Salir = tk.Button(ventana, text="Salir", command=cerrar_ventana) 
 boton_Salir.grid(row=7, column=0)
-
-
-
 
 ventana.mainloop()
